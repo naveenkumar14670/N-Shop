027618e45e1bac5faa34ef4fa222d1a1d4b98b33
Naveen Anagandula <naveenvineeth6@gmail.com>
2021-07-04 19:35:21 +0530

    minor change after deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCW1gOAqZStbS-WZgoygB3bBh2dvKBQ6CA',
+    authDomain: 'n-shop-7945f.firebaseapp.com',
+    projectId: 'n-shop-7945f',
+    storageBucket: 'n-shop-7945f.appspot.com',
+    messagingSenderId: '441245940058',
+    appId: '1:441245940058:web:e11c7b9e48d88e6b329b63',
+    measurementId: 'G-1WZD69LBJJ',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: 'AIzaSyCW1gOAqZStbS-WZgoygB3bBh2dvKBQ6CA',
     authDomain: 'n-shop-7945f.firebaseapp.com',
     projectId: 'n-shop-7945f',
     storageBucket: 'n-shop-7945f.appspot.com',
     messagingSenderId: '441245940058',
-    // appId: '1:441245940058:web:e11c7b9e48d88e6b329b63',
-    // measurementId: 'G-1WZD69LBJJ',
+    appId: '1:441245940058:web:e11c7b9e48d88e6b329b63',
+    measurementId: 'G-1WZD69LBJJ',
   },
 };
 
